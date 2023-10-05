b9147ad6671c39dad99e13accbe077d97e9cf7a8
Javi <javierortegacobos@gmail.com>
2023-10-05 11:16:30 +0200

    cv del fary

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/', function (req, res) {
-  res.sendFile('cv.html')
-})
+  res.sendFile(path.join(__dirname, 'DESPLIEGUERENDER/cv.html'));
+});
 
 app.listen(3000)
